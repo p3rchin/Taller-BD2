9197b3ed872fae1aede23a1d903719e05985a6f6
arnunez2002 <70178954+arnunez2002@users.noreply.github.com>
2022-03-18 10:20:19 -0500

    Update Usuario.java
    Setters y getter de usuario

--- src/co/edu/unbosque/model/Usuario.java
+++ src/co/edu/unbosque/model/Usuario.java
@@ -2,4 +2,93 @@ package co.edu.unbosque.model;
 
 public class Usuario {
 
+	private String id_usuario;
+	private String nombre;
+	private String apellidos;
+	private String telefono;
+	private String direccion;
+	private String sexo;
+	private int saldo;
+	private String estado;
+
+	public Usuario() {
+
+	}
+
+	public Usuario(String id_usuario, String nombre, String apellidos, String telefono, String direccion, String sexo,
+			int saldo, String estado) {
+		this.id_usuario = id_usuario;
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+		this.telefono = telefono;
+		this.direccion = direccion;
+		this.sexo = sexo;
+		this.saldo = saldo;
+		this.estado = estado;
+	}
+
+	public String getId_usuario() {
+		return id_usuario;
+	}
+
+	public void setId_usuario(String id_usuario) {
+		this.id_usuario = id_usuario;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public String getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+
+	public int getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(int saldo) {
+		this.saldo = saldo;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
 }
