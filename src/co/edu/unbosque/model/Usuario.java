6bf2990080a33b42de8dccd5ef3dd2902f770b6b
RobinsonF <rgutierrezs@unbosque.edu.co>
2022-03-20 11:49:03 -0500

    Metodo para crear a un usuario

--- src/co/edu/unbosque/model/Usuario.java
+++ src/co/edu/unbosque/model/Usuario.java
@@ -6,6 +6,7 @@ public class Usuario {
 	private String nombre;
 	private String apellidos;
 	private String telefono;
+	private String correo;
 	private String direccion;
 	private String sexo;
 	private int saldo;
@@ -15,78 +16,141 @@ public class Usuario {
 
 	}
 
-	public Usuario(String id_usuario, String nombre, String apellidos, String telefono, String direccion, String sexo,
-			int saldo, String estado) {
+	public Usuario(String id_usuario, String nombre, String apellidos, String telefono, String correo, String direccion,
+			String sexo, int saldo, String estado) {
 		this.id_usuario = id_usuario;
 		this.nombre = nombre;
 		this.apellidos = apellidos;
 		this.telefono = telefono;
+		this.correo = correo;
 		this.direccion = direccion;
 		this.sexo = sexo;
 		this.saldo = saldo;
 		this.estado = estado;
 	}
 
+	/**
+	 * @return the id_usuario
+	 */
 	public String getId_usuario() {
 		return id_usuario;
 	}
 
+	/**
+	 * @param id_usuario the id_usuario to set
+	 */
 	public void setId_usuario(String id_usuario) {
 		this.id_usuario = id_usuario;
 	}
 
+	/**
+	 * @return the nombre
+	 */
 	public String getNombre() {
 		return nombre;
 	}
 
+	/**
+	 * @param nombre the nombre to set
+	 */
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
 
+	/**
+	 * @return the apellidos
+	 */
 	public String getApellidos() {
 		return apellidos;
 	}
 
+	/**
+	 * @param apellidos the apellidos to set
+	 */
 	public void setApellidos(String apellidos) {
 		this.apellidos = apellidos;
 	}
 
+	/**
+	 * @return the telefono
+	 */
 	public String getTelefono() {
 		return telefono;
 	}
 
+	/**
+	 * @param telefono the telefono to set
+	 */
 	public void setTelefono(String telefono) {
 		this.telefono = telefono;
 	}
 
+	/**
+	 * @return the correo
+	 */
+	public String getCorreo() {
+		return correo;
+	}
+
+	/**
+	 * @param correo the correo to set
+	 */
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
+
+	/**
+	 * @return the direccion
+	 */
 	public String getDireccion() {
 		return direccion;
 	}
 
+	/**
+	 * @param direccion the direccion to set
+	 */
 	public void setDireccion(String direccion) {
 		this.direccion = direccion;
 	}
 
+	/**
+	 * @return the sexo
+	 */
 	public String getSexo() {
 		return sexo;
 	}
 
+	/**
+	 * @param sexo the sexo to set
+	 */
 	public void setSexo(String sexo) {
 		this.sexo = sexo;
 	}
 
+	/**
+	 * @return the saldo
+	 */
 	public int getSaldo() {
 		return saldo;
 	}
 
+	/**
+	 * @param saldo the saldo to set
+	 */
 	public void setSaldo(int saldo) {
 		this.saldo = saldo;
 	}
 
+	/**
+	 * @return the estado
+	 */
 	public String getEstado() {
 		return estado;
 	}
 
+	/**
+	 * @param estado the estado to set
+	 */
 	public void setEstado(String estado) {
 		this.estado = estado;
 	}
---
+++ src/co/edu/unbosque/model/persistences/UsuarioDAO.java
@@ -0,0 +1,49 @@
+package co.edu.unbosque.model.persistences;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import co.edu.unbosque.model.Usuario;
+
+public class UsuarioDAO extends ManejoConexion {
+
+	public UsuarioDAO() {
+		try {
+            establecerConexion();
+        } catch (SQLException ex) {
+			Logger.getLogger(UsuarioDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+	}
+	
+	 public boolean crearCliente(Usuario usuario) {
+	        try {
+
+	            String comandoSQL = "INSERT INTO USUARIO (ID_USUARIO, NOMBRE, APELLIDO, TELEFONO, CORREO, DIRECCION, SEXO, SALDO, ESTADO) VALUES (?,?,?,?,?,?,?,?,?)";
+	            if (!conexion.isClosed()) {
+
+	                PreparedStatement ps = conexion.prepareStatement(comandoSQL);
+	                ps.setString(1, usuario.getId_usuario());
+	                ps.setString(2, usuario.getNombre());
+	                ps.setString(3, usuario.getApellidos());
+	                ps.setString(4, usuario.getTelefono());
+	                ps.setString(5, usuario.getCorreo());
+	                ps.setString(6, usuario.getDireccion());
+	                ps.setString(7, usuario.getSexo());
+	                ps.setInt(8, usuario.getSaldo());
+	                ps.setString(9, usuario.getEstado());
+
+
+	                return ps.executeUpdate() > 0;
+	            } else {
+	                return false;
+	            }
+
+	        } catch (SQLException ex) {
+	            Logger.getLogger(UsuarioDAO.class.getName()).log(Level.SEVERE, null, ex);
+	            return false;
+	        }
+
+	    }
+}
